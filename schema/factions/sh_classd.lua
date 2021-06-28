0128c653cf9749c54511d3e6c5cb9fea119fd89c
stealthily <joeyrynkiewicz1996@gmail.com>
2021-06-28 12:06:44 -0400

    General Update
    Added Class-D Models, added Security Department + Models

--- schema/factions/sh_classd.lua
+++ schema/factions/sh_classd.lua
@@ -6,10 +6,24 @@ FACTION.name = "Class-D Personnel"
 FACTION.description = "Class D personnel are expendable personnel used to handle extremely hazardous anomalies."
 FACTION.isDefault = true
 FACTION.color = Color(255, 180, 0)
+FACTION.pay = 10
+FACTION.isGloballyRecognized = true
 
 -- You should define a global variable for this faction's index for easy access wherever you need. FACTION.index is
 -- automatically set, so you can simply assign the value.
 
 -- Note that the player's team will also have the same value as their current character's faction index. This means you can use
 -- client:Team() == FACTION_CITIZEN to compare the faction of the player's current character.
+FACTION.models = {
+	"models/cultist/class_d_2.mdl"
+	"models/cultist/class_d_3.mdl"
+	"models/cultist/class_d_4.mdl"
+	"models/cultist/class_d_5.mdl"
+	"models/cultist/class_d_6.mdl"
+	"models/cultist/class_d_7.mdl"
+	"models/cultist/class_d_8.mdl"
+	"models/cultist/class_d_9.mdl"
+}
+
 FACTION_CLASSD = FACTION.index
+
---
+++ schema/factions/sh_security.lua
@@ -0,0 +1,24 @@
+
+-- Since this faction is not a default, any player that wants to become part of this faction will need to be manually
+-- whitelisted by an administrator.
+
+FACTION.name = "Security Department"
+FACTION.description = "This department is responsible for providing security and protection for all Foundation facilities and objects, regardless of their priority."
+FACTION.color = Color(20, 120, 185)
+FACTION.pay = 50 -- How much money every member of the faction gets paid at regular intervals.
+FACTION.weapons = {"weapon_pistol"} -- Weapons that every member of the faction should start with.
+FACTION.isGloballyRecognized = true -- Makes it so that everyone knows the name of the characters in this faction.
+
+-- Note that FACTION.models is optional. If it is not defined, it will use all the standard HL2 citizen models.
+FACTION.models = {
+	"models/armacham/security2/guard_1.mdl"
+	"models/armacham/security2/guard_2.mdl"
+	"models/armacham/security2/guard_3.mdl"
+	"models/armacham/security2/guard_4.mdl"
+	"models/armacham/security2/guard_5.mdl"
+	"models/armacham/security2/guard_7.mdl"
+	"models/armacham/security2/guard_8.mdl"
+	"models/armacham/security2/guard_9.mdl"
+}
+
+FACTION_SECURITY = FACTION.index
